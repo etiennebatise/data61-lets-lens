f64304987768c881d184dba3e669ab0968bfb34b
Etienne Batise <etienne.batise@gmail.com>
2018-07-17 16:02:01 +0200

    upadte Profunctor - indent / style

--- src/Lets/Profunctor.hs
+++ src/Lets/Profunctor.hs
@@ -7,15 +7,10 @@ import Lets.Data
 -- | A profunctor is a binary functor, with the first argument in contravariant
 -- (negative) position and the second argument in covariant (positive) position.
 class Profunctor p where
-  dimap ::
-    (b -> a)
-    -> (c -> d)
-    -> p a c
-    -> p b d
+  dimap :: (b -> a) -> (c -> d) -> p a c -> p b d
 
 instance Profunctor (->) where
   dimap f g = \h -> g . h . f
 
 instance Profunctor Tagged where
-  dimap _ g (Tagged x) =
-    Tagged (g x)
+  dimap _ g (Tagged x) = Tagged (g x)
